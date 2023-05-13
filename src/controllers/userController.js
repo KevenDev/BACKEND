fd1a4c9a41da0e5e08bab17e3883a155d73d3c2f
Keven Teles <glevenkeven@gmail.com>
2023-05-12 23:27:06 -0300

    feat: new Controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ const prisma = require('../databases')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+
 const getAllUsers = async (req,res) =>{
   const user = await prisma.user.findMany()
   return res.status(200).json(user)
@@ -53,6 +54,63 @@ const newUser = async (req,res) =>{
   }
 }
 
+const loginUser = async(req,res)=>{
+  const {email,password} = req.body
+
+  
+  if(!email){
+    return res.status(422).json({msg: 'O Email é obrigatório'})
+  }
+  if(!password){
+    return res.status(422).json({msg: 'A Senha é obrigatório'})
+  }
+
+  const user = await prisma.user.findFirst({
+    where:{
+      email: email
+    }
+  })
+
+  //checar senha 
+  const checkPassword = await bcrypt.compare(password, user.password)
+
+  if(!checkPassword){
+    return res.status(422).json({msg: "Senha Inválida"})
+  }
+
+  if(!user){
+    return res.status(400).json({msg: "usuário não encontrado"})
+  }
+
+  try {
+    const secret = process.env.SECRET
+
+    const token = jwt.sign({
+      id: user._id
+    },secret,
+    )
+
+    res.status(200).json({msg: "Autenticação realizada com sucesso", token})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({msg: "Aconteceu um erro no servidor, tente novamente mais tarde"})
+  }
+}
+
+const getUserId = async(req,res)=>{
+  const {id} = req.params
+
+  const user = await prisma.user.findFirst({
+    where:{
+      id: Number(id)
+    }
+  })
+  if(!user){
+    return res.status(404).json({msg: "usuário não encontrado"})
+  }
+  res.status(200).json({user})
+}
+
 const removeUser = async(req, res) =>{
   const {id} = req.params
   const user = await prisma.user.delete({
@@ -62,8 +120,11 @@ const removeUser = async(req, res) =>{
   })
 }
 
+
 module.exports = {
   getAllUsers,
   newUser,
-  removeUser
+  removeUser,
+  loginUser,
+  getUserId
 }
