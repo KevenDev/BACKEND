ff6286e2dda31de6e85ce4474a526cc37680443b
Keven Teles <glevenkeven@gmail.com>
2023-05-12 23:27:26 -0300

    feat: new route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const  Router  = require("express");
 const userController = require("../controllers/userController");
 const productController =  require("../controllers/productController");
 const router = new Router()
+const checkToken = require("../middleware/checkToken")
 
 
 //listar todos os usuarios 
@@ -10,8 +11,15 @@ router.get('/user', userController.getAllUsers)
 //registrar novo usuário
 router.post('/user/register', userController.newUser)
 
+//login do usuário
+router.post('/user/login', userController.loginUser)
+
 //deletar usuário
 router.post('/user/delete/:id', userController.removeUser)
+
+//Private Route
+router.get('/user/:id', checkToken, userController.getUserId)
+
 
 router.get('/product', productController.getAllProducts)
 router.get('/product/:authorId', productController.getProductsId)
